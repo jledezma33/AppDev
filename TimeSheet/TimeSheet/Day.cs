b77e26dbe8f48a1c44276b4ff5213e5c82983db3
Jesus Ledezma <jledezma@student.neumont.edu>
2016-06-27 11:30:40 -0600

    Updated day class.

--- TimeSheet/TimeSheet/Day.cs
+++ TimeSheet/TimeSheet/Day.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace TimeSheet
 {
-    class Day
+    public class Day
     {
         public DateTime Date { get; set; }
 
         public float HoursWorked { get; set; }
 
         //time codes sick, vacation, regular
-        
+        public enum TimeCodes { sick, vacation, regular}
+
+        public string sick = TimeCodes.sick.ToString();
     }
 }

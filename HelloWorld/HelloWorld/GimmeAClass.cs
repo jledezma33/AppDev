c4a3282ec0ae031c7410d3a4534b03e7f6c51e17
Jesus Ledezma <jledezma@student.neumont.edu>
2016-06-27 08:34:07 -0600

    Initial commit.

---
+++ HelloWorld/HelloWorld/GimmeAClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class GimmeAClass
+    {
+        public GimmeAClass()
+        {
+
+        }
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string AnotherName { get; set; }
+    }
+}
---
+++ creatingfiles/creatingfiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+
+
+            string[] data = new string[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i % 1000 == 0) { Console.WriteLine(i); }
+
+                data[i] = Guid.NewGuid().ToString();
+            }
+
+            timer.Start();
+            File.AppendAllLines(@"c:\temp\mydatawrittenonce.txt", data);
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+        }
+    }
+}

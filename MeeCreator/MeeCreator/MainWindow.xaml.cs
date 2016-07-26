683c4f90d25bb3e075301eb95e50ca893b9961d4
Jesus Ledezma <jledezma@student.neumont.edu>
2016-07-26 08:58:43 -0600

    Added stuff to MeeCreator/MainWindow.xaml

--- MeeCreator/MeeCreator/MainWindow.xaml.cs
+++ MeeCreator/MeeCreator/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace MeeCreator
         {
             InitializeComponent();
         }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            Ellipse myEllipse = new Ellipse();
+            myEllipse.Width = 500;
+            myEllipse.Height = 10;
+            myEllipse.Fill = Brushes.Blue;
+
+            FaceCanvas.Children.Add(myEllipse);
+        }
     }
 }
